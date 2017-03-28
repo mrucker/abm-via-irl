c3dcf374e8c4f4667d1870f8c7986cc1434dd66a
Kamwoo Lee <seankwlee@gmail.com>
2017-03-28 19:11:40 -0400

    add status 14 18 22

--- Matlab/Segregation/T_SA.m
+++ Matlab/Segregation/T_SA.m
@@ -102,15 +102,23 @@ function projection = Assign(P)
     end
     
     % model probability: action 2 in state 10
-    % -> assign to [action, state] = [1,6] [2,6] [1,10] [4,10]
-    a_in_s = [[1,6]; [2,6]; [1,10]; [4,10]];
+    % -> assign to [action, state] = [1,6] [2,6] [1,10] [1,14] [2,14] [1,18] [2,18] [1,22] [2,22] [4 22]
+    a_in_s = [[1,6]; [2,6]; [1,10]; [1,14]; [2,14]; [1,18]; [2,18]; [1,22]; [2,22]; [4 22]];
     for i=1:size(a_in_s, 1)
         [a, s] = deal(a_in_s(i,1), a_in_s(i,2));
         P{a}(s,:) = P{2}(10,:);
     end
     
+    % model probability: action 4 in state 6
+    % -> assign to [action, state] = [4,10] [4,14] [4,18]
+    a_in_s = [[4,10]; [4,14]; [4,18]];
+    for i=1:size(a_in_s, 1)
+        [a, s] = deal(a_in_s(i,1), a_in_s(i,2));
+        P{a}(s,:) = P{4}(6,:);
+    end
+    
     % assign probability for illegal actions
-    action = 3; from = [1,6,10,3,8,12,16,20,24];    to = [25];  prob = 1;
+    action = 3; from = [1,6,10,14,18,22,3,8,12,16,20,24];    to = [25];  prob = 1;
     P{action}(from, to) = prob;
 
     action = 4; from = [1,2,3,4];   to = [25];  prob = 1;
@@ -118,24 +126,12 @@ function projection = Assign(P)
 
     % assign probability for unreachable state
     action = [1,2,3,4]; 
-    from = [5,9,13,17,21,7,11,15,19,23,14,18,22,25];   to = [25];  prob = 1;
+    from = [5,9,13,17,21,7,11,15,19,23,25];   to = [25];  prob = 1;
     for a = 1:length(action)
         P{a}(from, to) = prob;
     end
     
     projection = P;
-    
-    
-%     %get default transitions and then overwrite them
-%     default = T_2_1(num_actions, num_states);
-%     
-%     for a = 1:num_actions
-%         for s = 1:num_states
-%             if(sum(P{a}(s,:)) > 0)
-%                 default{a}(s,:) = P{a}(s,:);
-%             end
-%         end
-%     end
 end
 
 function Validate(P, num_actions, num_states)
