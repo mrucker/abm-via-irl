69799d85f7f78e5017beab3683717f0b3ea010c8
hahayonghuming <js9wv@virginia.edu>
2017-04-03 11:11:00 -0400

    fix

---
+++ Matlab/Segregation2/elbowPlot.m
@@ -0,0 +1,9 @@
+function elbowPlot(P)
+
+% elbowPlot visualize the P to choose the right k
+% Argument-----------------------------
+% P= a vector of percentage of variances explained by each k
+
+plot(P);
+end
+
