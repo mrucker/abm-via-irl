fbafac2163ae5b8375b76bb764a4a42056e1c98c
Mark Rucker <rucker.mark@gmail.com>
2017-04-14 22:37:37 -0400

    better T_LDA

--- Matlab/Segregation2_5/T_LDA.m
+++ Matlab/Segregation2_5/T_LDA.m
@@ -47,8 +47,13 @@ end
 
 function dlt_predictors = PredictorDeltas(AS)
     dlt_predictors = AS(2:end, 2:end) - AS(1:end-1, 2:end);
-    lt0_predictors = dlt_predictors(:,1) < 0;
-    dlt_predictors(lt0_predictors, 1) = -100; 
+    lt0_1_predictors = dlt_predictors(:,1) <= 0;
+    nt0_2_predictors = dlt_predictors(:,2) ~= 0;
+    nt0_3_predictors = dlt_predictors(:,3) ~= 0;
+    
+    dlt_predictors(lt0_1_predictors, 1) = -100;
+    dlt_predictors(nt0_2_predictors, 2) = 1; 
+    dlt_predictors(nt0_3_predictors, 3) = 1; 
 end
 
 function cls_predictors = PredictorClasses(dlt_predictors)
@@ -62,28 +67,33 @@ end
 
 function fit_predictors = PredictorFits(AS, dlt_predictors)
     num_predicotrs = size(dlt_predictors,2);    
-    fit_predictors = cell(1,num_predicotrs);    
+    fit_predictors = cell(1,num_predicotrs);
 
     for i = 1:num_predicotrs
-        fit_predictors{i} = fitcdiscr(AS(1:end-1, :), dlt_predictors(:,i));
+        fit_predictors{i} = LDA(AS(1:end-1, :), dlt_predictors(:,i));
     end
 end
 
 function ts = Transitions(a, s, fit_predictors, cls_predictors)
-            ts = cartesian(1, cls_predictors{:});
-            
-            for p = 1:numel(s)
-                [~, score, ~] = predict(fit_predictors{p}, [a, s']);                
-                
-                for c = vertcat(fit_predictors{p}.ClassNames', score)                    
-                    ts(ts(:,p+1) == c(1), 1) = ts(ts(:,p+1) == c(1), 1) * c(2);
-                end
-            end
-            
-            ts(:, 2:end) = ts(:, 2:end) + s';            
-            
-            ts(ts(:,2) > 5, 2) = 5;
-            ts(ts(:,2) < 0, 2) = 0;
+    ts = cartesian(1, cls_predictors{:});
+
+    for i = 1:numel(s)
+        for c = fit_predictors{i}([a, s'])                    
+            ts(ts(:,i+1) == c(1), 1) = ts(ts(:,i+1) == c(1), 1) * c(2);
+        end
+    end
+
+    for r = 1:size(ts,1)
+        for c = 2:size(ts,2)
+            ts(r,c) = Change(c-1, s(c-1), ts(r,c));
+        end
+    end
+    
+%     ts(:, 2:end) = ts(:, 2:end) + s';
+% 
+%     ts(ts(:,2) > 5, 2) = 5;
+%     ts(ts(:,2) < 0, 2) = 0;
+
 end
 
 function P = Initialize(num_actions, num_states)
@@ -134,4 +144,26 @@ function P = Validate(p, num_actions, num_states)
     end
     
     P = p;
+end
+
+function lda = LDA(training, responses)
+    fit = fitcdiscr(training, responses);
+    lda = @(predictors) Prediction(fit, predictors);
+end
+
+function prediction = Prediction(fit, predictors)
+    [~, score, ~] = predict(fit, predictors);
+    prediction = vertcat(fit.ClassNames', score);
+end
+
+function change = Change(predictor_i, predictor_v, class)
+    if predictor_i == 1 && class == -100
+        change = 0;
+    elseif predictor_i == 1 && class == 1 && predictor_v == 5
+        change = 5;     
+    elseif ismember(predictor_i, [2 3]) && class == 1
+        change = abs(predictor_v - 1);
+    else
+        change = predictor_v + class;
+    end
 end
