458c8aea8862e2cf8a5bd8c23b5c68056d1f22c0
Kamwoo Lee <kamwoolee@d-172-25-44-106.dhcp.virginia.edu>
2017-04-16 15:49:15 -0400

    policy simulation

--- Matlab/Segregation2_2/save_learned_policy.m
+++ Matlab/Segregation2_2/save_learned_policy.m
@@ -1,18 +1,33 @@
-function save_learned_policy(output_file, num_clusters, group_idx, policies)
+function save_learned_policy(output_file, num_clusters, group_idx, state_space, policies)
 
     new_file(output_file);
     
+    [num_states, num_state_variables] = size(state_space);
+    state_space = horzcat((1:num_states)', state_space);
+    
+    head = 'number of states';
+    write_head(output_file, head);
+    dlmwrite(output_file, num_states, '-append'); 
+    
+    head = 'number of state variables';
+    write_head(output_file, head);
+    dlmwrite(output_file, num_state_variables, '-append'); 
+    
+    head = 'state space';
+    write_head(output_file, head);
+    dlmwrite(output_file, state_space, '-append'); 
+
     head = 'number of clusters';
     write_head(output_file, head);
     dlmwrite(output_file, num_clusters, '-append'); 
     
     head = 'number of agents in each cluster';
     write_head(output_file, head);
-    data = zeros(1, num_clusters);
+    num_agents = zeros(1, num_clusters);
     for i=1:num_clusters
-        data(i) = length(group_idx{i});
+        num_agents(i) = length(group_idx{i});
     end
-    dlmwrite(output_file, data, '-append');
+    dlmwrite(output_file, num_agents, '-append');
     
     for i=1:num_clusters
         head = ['stochastic policy for cluster',num2str(i)];
